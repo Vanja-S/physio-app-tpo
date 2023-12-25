e5e2932ad884966529df2bc585995307a42c5e0f
Tadej Delopst <td7908@student.uni-lj.si>
2023-12-25 21:58:40 +0100

    initial server implementation

---
+++ fizio-app/src/main/java/com/tpo/fizio/FizioAppApplication.java
@@ -0,0 +1,13 @@
+package com.tpo.fizio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizioAppApplication.class, args);
+	}
+
+}
---
+++ fizio-app/src/test/java/com/tpo/fizio/FizioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpo.fizio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
